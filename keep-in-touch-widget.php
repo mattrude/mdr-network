ffb6b7254ba4eb63c3099ffb010274ea5d040b75
Matt Rude <m@mattrude.com>
2010-09-12 15:38:27 -0500

    Adding Icon images and CSS

--- keep-in-touch-widget.php
+++ keep-in-touch-widget.php
@@ -14,6 +14,14 @@ class milly_keep_in_touch_widget extends WP_Widget {
     $twitter_id = strip_tags($instance['twitter_id']);
     $facebook_id = strip_tags($instance['facebook_id']);
     echo "{$before_widget}{$before_title}$widget_title{$after_title}<ul class='keep-in-touch'>";
+    echo "<style type='text/css'>";
+    echo "  .keep-in-touch li#feed { background: url(/wp-content/mu-plugins/images/keep-in-touch.png) no-repeat scroll 0 1px transparent; }";
+    echo "  .keep-in-touch li#twitter { background: url(/wp-content/mu-plugins/images/keep-in-touch.png) no-repeat scroll 0 -28px transparent; }";
+    echo "  .keep-in-touch li#facebook { background: url(/wp-content/mu-plugins/images/keep-in-touch.png) no-repeat scroll 0 -58px transparent; }";
+    echo "  .keep-in-touch li#feed a { padding: 0 0 0 10px; }";
+    echo "  .keep-in-touch li#twitter a { padding: 0 0 0 10px; }";
+    echo "  .keep-in-touch li#facebook a { padding: 0 0 0 10px; }";
+    echo "</style>";
     echo "<li id='twitter'><a href='http://twitter.com/$twitter_id'>Twitter</a></li>";
     echo "<li id='facebook'><a href='http://facebook.com/$facebook_id'>Facebook</a></li>";
     echo "<li id=feed><a href='" .get_bloginfo_rss('atom_url'). "'>ATOM</a> |<a href='" .get_bloginfo_rss('rss2_url'). "'>RSS</a> Post Feeds</li>";
@@ -34,7 +42,8 @@ class milly_keep_in_touch_widget extends WP_Widget {
     $widget_title = strip_tags($instance['widget_title']);
     $twitter_id = strip_tags($instance['twitter_id']);
     $facebook_id = strip_tags($instance['facebook_id']);
-    ?><p><label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget Title')?>:<input class="widefat" id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" type="text" value="<?php echo esc_attr($widget_title); ?>" /></label></p><?php
+    ?>
+    <p><label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget Title')?>:<input class="widefat" id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" type="text" value="<?php echo esc_attr($widget_title); ?>" /></label></p><?php
     ?><p><label for="<?php echo $this->get_field_id('twitter_id'); ?>"><?php _e('Twitter ID')?>:<input class="widefat" id="<?php echo $this->get_field_id('twitter_id'); ?>" name="<?php echo $this->get_field_name('twitter_id'); ?>" type="text" value="<?php echo esc_attr($twitter_id); ?>" /></label></p><?php
     ?><p><label for="<?php echo $this->get_field_id('facebook_id'); ?>"><?php _e('Facebook ID')?>:<input class="widefat" id="<?php echo $this->get_field_id('facebook_id'); ?>" name="<?php echo $this->get_field_name('facebook_id'); ?>" type="text" value="<?php echo esc_attr($facebook_id); ?>" /></label></p><?php
   }
