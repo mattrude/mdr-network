b1ef861470b5d99609034afcf241126bb3b797ba
Matt Rude <m@mattrude.com>
2010-09-16 12:17:38 -0500

    more sitemap updates

--- templates/feed-sitemap.php
+++ templates/feed-sitemap.php
@@ -9,8 +9,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 <urlset	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
 	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-	xmlns:image="http://www.sitemaps.org/schemas/sitemap-image/1.1"
-        xmlns:video="http://www.sitemaps.org/schemas/sitemap-video/1.1"
+	xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
 >
 	<url>
 		<loc><?php bloginfo_rss('url') ?></loc>
@@ -56,17 +55,18 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
                 'post_type' => 'attachment',
                 'numberposts' => 100,
 		'post_parent' => $post->ID,
+		'post_mime_type' => 'image',
                 'orderby' => 'date',
                 'order' => 'DESC'
         );
-	$attachments = get_posts($args2);
-	if ($attachments) {
-		foreach ($attachments as $post) { ?>
+	$images = get_posts($args2);
+	if ($images) {
+		foreach ($images as $post) { ?>
 		<image:image>
-			<image:image><?php echo wp_get_attachment_url(); ?></image:loc>
+			<image:loc><?php echo wp_get_attachment_url(); ?></image:loc>
 <?php if ( !empty($post->post_excerpt) ) echo '			<image:caption>' . $post->post_excerpt . '</image:caption>
 '; ?>
-			<image:title><?php echo the_title(); ?></image:title>
+			<image:title><?php echo $post->post_title ?></image:title>
 		</image:image>
 <?php } } ?>
 		<lastmod><?php echo mysql2date('Y-m-d\TH:i:s\Z', get_post_time('Y-m-d H:i:s', true), false); ?></lastmod>
