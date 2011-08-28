e705df29b8275a128b198a4a0a178e5868bc326e
Matt Rude <matt@mattrude.com>
2011-08-28 12:12:18 -0500

    Added 'Picasa Captioner' plugin to allow WordPress to read Picasa Captions from EXIF info properly.

---
+++ picasa-captioner.php
@@ -0,0 +1,27 @@
+<?php
+/**
+Plugin Name: Picasa Captioner
+Description: Fix up WordPress to read Picasa Captions from EXIF info properly.
+Plugin URI: http://ottopress.com/2011/picasa-and-wordpress-and-captions/
+Author: Otto
+Author URI: http://ottodestruct.com/
+**/
+
+add_filter( 'wp_read_image_metadata', 'picasa_adjust_caption' );
+function picasa_adjust_caption($meta) {
+	if (empty($meta['caption']) && !empty($meta['title'])) {
+		$meta['caption'] = $meta['title'];
+		$meta['title'] = '';
+	}
+	return $meta;
+}
+
+add_action( 'add_attachment', 'picasa_adjust_attachment' );
+function picasa_adjust_attachment($id) {
+	$attachment = & get_post( $id, ARRAY_A );
+	if ( !empty( $attachment ) ) {
+		$attachment['post_excerpt'] = $attachment['post_content'];
+		$attachment['post_content'] = '';
+		wp_update_post($attachment);
+	}
+} ?>
